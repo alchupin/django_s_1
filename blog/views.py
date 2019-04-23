812379c13e79700973f526608be276c9382b9ac3
alchupin <ximikk@mail.ru>
2019-04-23 10:47:47 -0700

    class based views result

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import get_object_or_404
 
 
 from .models import Post, Tag
+from .utils import ObjectDetailMixin
 
 
 def posts_list(request):
@@ -20,11 +21,14 @@ def tags_list(request):
 #     post = Post.objects.get(slug__exact=slug)
 #     return render(request, 'blog/post_detail.html', context={'post': post})
 
-class PostDetail(View):
-    def get(self, request, slug):
-        #post = Post.objects.get(slug__exact=slug)
-        post = get_object_or_404(Post, slug__exact=slug)
-        return render(request, 'blog/post_detail.html', context={'post': post})
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+    # def get(self, request, slug):
+    #     #post = Post.objects.get(slug__exact=slug)
+    #     post = get_object_or_404(Post, slug__exact=slug)
+    #     return render(request, 'blog/post_detail.html', context={'post': post})
 
 
 
@@ -32,8 +36,11 @@ class PostDetail(View):
 #     tag = Tag.objects.get(slug__iexact=slug)
 #     return render(request, 'blog/tag_detail.html', context={'tag':tag})
 
-class TagDetail(View):
-    def get(self, request, slug):
-        #tag = Tag.objects.get(slug__iexact=slug)
-        tag = get_object_or_404(Tag, slug__exact=slug)
-        return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+    # def get(self, request, slug):
+    #     #tag = Tag.objects.get(slug__iexact=slug)
+    #     tag = get_object_or_404(Tag, slug__exact=slug)
+    #     return render(request, 'blog/tag_detail.html', context={'tag': tag})
